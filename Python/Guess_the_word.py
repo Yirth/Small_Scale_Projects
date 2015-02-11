648a555ce8cc9af7f99b78b65748d068eb31c5dc
Yirth <jandersen752@outlook.com>
2015-02-11 12:01:56 -0500

    Fixed for Caps
    The problem with this program is that it works like a conventional lock.
    The first tumbler must go up and then you can move on to the second one.
    The problem being that you have to evaluate each character separately.

--- Python/Guess_the_word.py
+++ Python/Guess_the_word.py
@@ -3,6 +3,8 @@
 #Documentation: Essentially, the user will enter a word phrase or any item and then the program will try and find out what that phrase is. It will run through all special characters and letters. To check for solution, it will just compare to the user entry.
 #Eventually, there will be a list for checking common things and also checking for unlimited length. As well, there will be implementation for only being able to check at certain points.
 
+import time
+
 def Phrase():
 	phrase = raw_input("What is the phrase you would like the computer to guess?")
 	try:
@@ -12,7 +14,7 @@ def Phrase():
 	return phrase
 	
 def Guess():
-	Char = ['a','s','d','f','q','w','e','r','t','y','u','i','o','p','g','h','j','k','l','z','x','c','v','b','n','m',',','.','/','1','2','3','4','5','6','7','8','9','0','!','@','#','$','%','^','&','*','(',')','-','_','+','=','`','~','<','>','?',':',';','\'','\"','[','{',']','}']
+	Char = ['a','s','d','f','q','w','e','r','t','y','u','i','o','p','g','h','j','k','l','z','x','c','v','b','n','m',',','.','/','1','2','3','4','5','6','7','8','9','0','!','@','#','$','%','^','&','*','(',')','-','_','+','=','`','~','<','>','?',':',';','\'','\"','\\','|','[','{',']','}','A','Q','W','E','R','T','Y','U','I','O','P','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M']
 	ToFind = Phrase()
 	PhraseList = list(ToFind)
 	Result = []
@@ -28,4 +30,6 @@ def Guess():
 					j+=1
 
 if __name__ == "__main__":
+	start = time.time()
 	Guess()
+	print( 'Time of execution: {0}'.format(time.time()-start))
